3d4841e61b07ca3d4bd41dc97ec69ef3178cbc6f
blkredstarv <rlcg55@gmail.com>
2016-05-02 21:11:07 -0500

    Add Maintainer
    Signed-off-by: blkredstarv <rlcg55@gmail.com>

--- broken.mk
+++ broken.mk
@@ -8,5 +8,9 @@ $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMF1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMF1 release-keys"
 
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Roberto Correa (RCG)"
+
 PRODUCT_NAME := broken_d2vzw
 PRODUCT_DEVICE := d2vzw
